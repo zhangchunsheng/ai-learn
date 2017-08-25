d6872c971df3ba6c732e2a210d5a901862bace0a
zhangchunsheng <zhangchunsheng423@gmail.com>
2017-08-25 09:39:10 +0800

    1.新功能:新增中文识别

--- chn/text_to_image.php
+++ chn/text_to_image.php
@@ -7,22 +7,38 @@ $path = $argv[1];
 
 $file = $path . "/train/啊.png";
 
+$width = 60;
+$height = 60;
+
 // Create the image
-$im = imagecreatetruecolor(60, 60);
+$im = imagecreatetruecolor($width, $height);
 
 // Create some colors
 $white = imagecolorallocate($im, 255, 255, 255);
 $grey = imagecolorallocate($im, 128, 128, 128);
 $black = imagecolorallocate($im, 0, 0, 0);
-imagefilledrectangle($im, 0, 0, 60, 60, $white);
+
+$left = 0;
+$right = 0;
+imagefilledrectangle($im, $left, $right, $width, $height, $white);
 
 // The text to draw
 $text = '啊';
 // Replace path by your own font path
 $font = 'font-type/micro_yahei.ttf';
 
+$size = 40;
+$angle = 0;
+$fontfile = $font;
+$bbox = imagettfbbox($size, $angle, $fontfile, $text);
+
+$dx = abs($bbox[2] - $bbox[0]);
+
+$px = abs($width / 2) - abs($dx / 2);
+$py =  abs($bbox[5] - $bbox[3]);
+
 // Add the text
-imagettftext($im, 40, 0, 10, 50, $black, $font, $text);
+imagettftext($im, $size, $angle, $px, $py, $black, $fontfile, $text);
 
 // Using imagepng() results in clearer text compared with imagejpeg()
 imagepng($im, $file);
