7473db16e1be9fe0d41b4c330a87c041548c7154
zhangchunsheng <zhangchunsheng423@gmail.com>
2017-08-25 15:03:52 +0800

    1.新功能:新增中文识别

--- chn/text_to_image.php
+++ chn/text_to_image.php
@@ -54,46 +54,82 @@ function drawTestText($character) {
     drawText($testPath, $character);
 }
 
+function makeDir($path) {
+    if (!is_dir($path)) {
+        if(!makeDir(dirname($path))) {
+            return false;
+        }
+        if(!mkdir($path, 0777)) {
+            return false;
+        } else {
+            chmod($path, 0777);
+        }
+    }
+    return true;
+}
+
 function drawText($path, $character) {
-    $file = $path . "/$character.png";
+    $path = $path . "/$character";
+    makeDir($path);
 
-    $width = 60;
-    $height = 60;
+    $count = 0;
 
-    // Create the image
-    $im = imagecreatetruecolor($width, $height);
+    // get ttf
+    $fontPath = 'font-type';
+    $fonts = array();
+    if(is_dir($fontPath)) {
+        $current_dir = opendir($fontPath);    //opendir()返回一个目录句柄,失败返回false
+        while(($file = readdir($current_dir)) !== false) {    //readdir()返回打开目录句柄中的一个条目
+            $sub_dir = $path . DIRECTORY_SEPARATOR . $file;    //构建子目录路径
+            if($file == '.' || $file == '..') {
+                continue;
+            } elseif(substr($file, stripos($file, ".") + 1) == "ttf") {    //如果是目录,进行递归
+                $fonts[] = $file;
+            }
+        }
+    }
 
-    // Create some colors
-    $white = imagecolorallocate($im, 255, 255, 255);
-    $grey = imagecolorallocate($im, 128, 128, 128);
-    $black = imagecolorallocate($im, 0, 0, 0);
+    foreach($fonts as $font) {
+        $file = $path . "/" . str_pad($count, 3, "0", STR_PAD_LEFT) . ".png";
 
-    $left = 0;
-    $right = 0;
-    imagefilledrectangle($im, $left, $right, $width, $height, $white);
+        $width = 60;
+        $height = 60;
 
-    // The text to draw
-    $text = "$character";
-    // Replace path by your own font path
-    $font = 'font-type/micro_yahei.ttf';
+        // Create the image
+        $im = imagecreatetruecolor($width, $height);
 
-    $size = 40;
-    $angle = 0;
-    $fontfile = $font;
-    $bbox = imagettfbbox($size, $angle, $fontfile, $text);
+        // Create some colors
+        $white = imagecolorallocate($im, 255, 255, 255);
+        $grey = imagecolorallocate($im, 128, 128, 128);
+        $black = imagecolorallocate($im, 0, 0, 0);
 
-    $dx = abs($bbox[2] - $bbox[0]);
-    $dy = abs($bbox[5] - $bbox[3]);
+        $left = 0;
+        $right = 0;
+        imagefilledrectangle($im, $left, $right, $width, $height, $white);
 
-    $px = abs($width / 2) - abs($dx / 2);
-    $py = abs($dy - (abs($height - $dy)) / 2);
+        // The text to draw
+        $text = "$character";
 
-    $py = $size + ($height - $size) / 2;
+        $size = 40;
+        $angle = 0;
+        $fontfile = $font;
+        $bbox = imagettfbbox($size, $angle, $fontfile, $text);
 
-    // Add the text
-    imagettftext($im, $size, $angle, $px, $py, $black, $fontfile, $text);
+        $dx = abs($bbox[2] - $bbox[0]);
+        $dy = abs($bbox[5] - $bbox[3]);
 
-    // Using imagepng() results in clearer text compared with imagejpeg()
-    imagepng($im, $file);
-    imagedestroy($im);
+        $px = abs($width / 2) - abs($dx / 2);
+        $py = abs($dy - (abs($height - $dy)) / 2);
+
+        $py = $size + ($height - $size) / 2;
+
+        // Add the text
+        imagettftext($im, $size, $angle, $px, $py, $black, $fontfile, $text);
+
+        // Using imagepng() results in clearer text compared with imagejpeg()
+        imagepng($im, $file);
+        imagedestroy($im);
+
+        $count++;
+    }
 }
