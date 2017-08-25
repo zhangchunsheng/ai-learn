20ac33bd22110f985ae4d2fded7f499534567e95
zhangchunsheng <zhangchunsheng423@gmail.com>
2017-08-25 09:06:44 +0800

    1.新功能:新增中文识别

--- chn/text_to_image.php
+++ chn/text_to_image.php
@@ -10,6 +10,12 @@ $file = $path . "/train/啊.png";
 // Create the image
 $im = imagecreatetruecolor(60, 60);
 
+// Create some colors
+$white = imagecolorallocate($im, 255, 255, 255);
+$grey = imagecolorallocate($im, 128, 128, 128);
+$black = imagecolorallocate($im, 0, 0, 0);
+imagefilledrectangle($im, 0, 0, 60, 60, $white);
+
 // The text to draw
 $text = '啊';
 // Replace path by your own font path
