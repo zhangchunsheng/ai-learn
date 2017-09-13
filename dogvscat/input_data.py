491751458b904aaf81ae2edd19469258923ff720
zhangchunsheng <zhangchunsheng423@gmail.com>
2017-09-13 16:01:31 +0800

    1.新功能:新增cats and dogs classifier

--- dogvscat/input_data.py
+++ dogvscat/input_data.py
@@ -35,3 +35,54 @@ def get_files(file_dir):
 
 # get_files("./data/train")
 
+def get_batch(image, label, image_W, image_H, batch_size, capacity):
+    image = tf.cast(image, tf.string);
+    label = tf.cast(label, tf.int32);
+
+    input_queue = tf.train.slice_input_producer([image, label])
+
+    image_contents = tf.read_file(input_queue[0])
+    label = input_queue[1]
+
+    image = tf.image.decode_jpeg(image_contents, channels=3);
+
+    #image = tf.image.resize_image_with_crop_or_pad(image, image_H, image_W)
+    #image = tf.image.per_image_standardization(image)
+    image = tf.image.resize_images(image, [image_H, image_W], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR);
+    #image = tf.cast(image, tf.float32)
+    image_batch, label_batch = tf.train.batch([image, label], batch_size=batch_size, num_threads=64, capacity=capacity);
+
+    #label_batch = tf.reshape(label_batch, [batch_size])
+
+    return image_batch, label_batch;
+
+import matplotlib.pyplot as plt;
+
+BATCH_SIZE = 2;
+CAPACITY = 256;
+IMG_W = 208;
+IMG_H = 208;
+
+train_dir = "./data/train";
+image_list, label_list = get_files(train_dir)
+image_batch, label_batch = get_batch(image_list, label_list, IMG_W, IMG_H, BATCH_SIZE, CAPACITY)
+
+with tf.Session() as sess:
+    i = 0;
+    coord = tf.train.Coordinator();
+    threads = tf.train.start_queue_runners(coord=coord)
+    try:
+        while not coord.should_stop() and i < 1:
+            img, label = sess.run([image_batch, label_batch])
+
+            for j, in np.arange(BATCH_SIZE):
+                print("label: %d" % label[j])
+                plt.imshow(img[j, :, :, :]);
+                plt.show()
+            i += 1;
+    except tf.errors.OutOfRangeError:
+        print("done!");
+    finally:
+        coord.request_stop()
+
+    coord.join(threads)
