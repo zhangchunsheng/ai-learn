029d3a9262521205abf98b2adbcf4173c21f531e
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-08-07 00:17:43 +0800

    learn pytorch

--- pytorch/2020/classifier.py
+++ pytorch/2020/classifier.py
@@ -24,3 +24,26 @@ Downloading https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz to ./data/ci
 Extracting ./data/cifar-10-python.tar.gz to ./data
 Files already downloaded and verified
 '''
+
+# pip install matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+# functions to show an image
+
+
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+# get some random training images
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+
+# show images
+imshow(torchvision.utils.make_grid(images))
+# print labels
+print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
