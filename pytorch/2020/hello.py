7fa73ff3a6e4e5ae4d6ba7f75eb80fcba6817954
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-08-06 23:11:48 +0800

    learn pytorch

--- pytorch/2020/hello.py
+++ pytorch/2020/hello.py
@@ -15,3 +15,17 @@ print(y)
 tensor([[3., 3.],
         [3., 3.]], grad_fn=<AddBackward0>)
 '''
+
+print(y.grad_fn)
+'''
+<AddBackward0 object at 0x7fdcd798a518>
+'''
+
+z = y * y * 3
+out = z.mean()
+
+print(z, out)
+'''
+tensor([[27., 27.],
+        [27., 27.]], grad_fn=<MulBackward0>) tensor(27., grad_fn=<MeanBackward0>)
+'''
