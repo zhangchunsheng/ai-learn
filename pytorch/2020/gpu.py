3c6ea9ee9da823f7a4989929b5aeda82cd971b0e
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-08-07 00:39:43 +0800

    learn pytorch

---
+++ pytorch/2020/gpu.py
@@ -0,0 +1,9 @@
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# Assuming that we are on a CUDA machine, this should print a CUDA device:
+
+print(device)
+
+net.to(device)
+
+inputs, labels = data[0].to(device), data[1].to(device)
