db52390b11ab58c94471fe5bab008d82510e7ea5
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-08-06 23:10:27 +0800

    learn pytorch

--- pytorch/2020/hello.py
+++ pytorch/2020/hello.py
@@ -2,3 +2,16 @@ import torch
 
 x = torch.ones(2, 2, requires_grad=True)
 print(x)
+
+'''
+tensor([[1., 1.],
+        [1., 1.]], requires_grad=True)
+'''
+
+y = x + 2
+print(y)
+
+'''
+tensor([[3., 3.],
+        [3., 3.]], grad_fn=<AddBackward0>)
+'''
